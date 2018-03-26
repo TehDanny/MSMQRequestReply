a73b86ef3517e351a5589866234cf82068ee292e
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-03-26 10:16:41 +0200

    Added projekts and classes

---
+++ Requestor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            Requestor requestor = new Requestor();
+        }
+    }
+}
